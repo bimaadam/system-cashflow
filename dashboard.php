6f36a2992df641946448c90017043b28a30fce08
bimaadam <rinbim.dev@gmail.com>
2025-08-15 13:14:00 +0700

    fix boostrap script

--- dashboard.php
+++ dashboard.php
@@ -15,7 +15,7 @@ $tab = $_GET['tab'] ?? 'dashboard';
 $title = 'Dashboard - Admin Dekorasi';
 
 // Set page title based on tab
-switch($tab) {
+switch ($tab) {
     case 'dashboard':
         $title = 'Dashboard - Admin Dekorasi';
         break;
@@ -45,15 +45,17 @@ if (in_array($tab, ['kas_masuk', 'kas_keluar', 'booking'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title><?= htmlspecialchars($title) ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+    <link href="" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 </head>
+
 <body class="sb-nav-fixed">
     <?php require 'layout/navbar.php'; ?>
     <div id="layoutSidenav">
@@ -79,12 +81,9 @@ if (in_array($tab, ['kas_masuk', 'kas_keluar', 'booking'])) {
     </div>
 
     <!-- Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/dist/js/bootstrap.bundle.js"></script>
     <script src="js/scripts.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
-    <script src="assets/demo/chart-area-demo.js"></script>
-    <script src="assets/demo/chart-bar-demo.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"></script>
     <?= $scripts_extra ?>
 </body>
+
 </html>
